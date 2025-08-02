3675788688ac2593493ca1a44c272bb557232d30
jorgepedro7 <jorgepedro7@gmail.com>
2025-08-02 03:06:52 +0000

    mostrando resultados 2

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import HomePage from "@/react-app/pages/Home";
-import Assessment from "@/react-app/pages/Assessment";
-import Result from "@/react-app/pages/Result";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Assessment from "./pages/Assessment";
+import Result from "./pages/Result";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/assessment" element={<Assessment />} />
-        <Route path="/result/:id" element={<Result />} />
+        <Route path="/" element={<Assessment />} />
+        <Route path="/result" element={<Result />} />
+        {/* Outras rotas podem ser adicionadas aqui */}
       </Routes>
     </Router>
   );
